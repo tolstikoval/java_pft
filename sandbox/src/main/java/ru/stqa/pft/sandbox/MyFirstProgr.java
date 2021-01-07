9e9bb30c4d787ef4b11122a62188491c3730a2e6
tolstikoval <61480444+tolstikoval@users.noreply.github.com>
2021-01-07 22:51:50 +0300

    Изменены настройки форматирования

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgr.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgr.java
@@ -1,8 +1,8 @@
 package ru.stqa.pft.sandbox;
 
-public class MyFirstProgr{
+public class MyFirstProgr {
 
- public static void main(String[] args) {
-  System.out.println("Hello, world!");
- }
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
 }
