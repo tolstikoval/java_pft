7b837e5be30d6e50967f07fb33e28fc620057bf3
tolstikoval <61480444+tolstikoval@users.noreply.github.com>
2021-01-11 01:53:41 +0300

    доработка + попытки работы с тестами

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgr.java
+++
@@ -1,8 +0,0 @@
-package ru.stqa.pft.sandbox;
-
-public class MyFirstProgr {
-
-    public static void main(String[] args) {
-        System.out.println("Hello, world!");
-    }
-}
---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -0,0 +1,22 @@
+package ru.stqa.pft.sandbox;
+
+import java.sql.SQLOutput;
+
+public class MyFirstProgram {
+
+    public static void main(String[] args) {
+        hello("world");
+        hello("user");
+        hello("Alina");
+
+        Square s = new Square(5);
+        System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
+
+        Rectangle r = new Rectangle(4, 6);
+        System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + "=" + r.area());
+        System.out.println("Hello, world!");
+    }
+    public static void hello(String somebody) {
+        System.out.println("Hello, " + somebody + "!");
+    }
+    }
